7f51d50f5787949164c563c42b7c055d8cb4ac46
limonsafayet <limon6531@gmail.com>
2021-03-29 12:34:55 +0600

    tutorial 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 
 function App() {
+  const title = "Weelcome to the new blog";
+  const likes = 50;
+  const person = {name: 'limon', age:30};
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
       </div>
     </div>
   );
