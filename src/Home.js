aaf1361e899b38ca3b91c291214af31ca2198db3
limonsafayet <limon6531@gmail.com>
2021-03-30 12:17:36 +0600

    tutorial 12

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList.js";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -9,12 +10,8 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter((blog => blog.author === 'mario'))} title="Mario's Blogs" />
     </div>
   );
 }
