a0ca57b99cc3b85a714657cfc09ea16581fbbec3
limonsafayet <limon6531@gmail.com>
2021-03-30 11:56:35 +0600

    tutorial 7

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,23 @@
-const Home = () => {
+import { useState } from "react";
 
-    const handleClick = () => {
-        console.log('hello')
-    }
+const Home = () => {
+  // let name = 'mario';
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    // name = 'luigi';
+    setName('luigi');
+    setAge(30);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <button onClick={handleClick}>Click</button>
+      <p>{ name } is { age } years old</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
-};
-
+}
+ 
 export default Home;
