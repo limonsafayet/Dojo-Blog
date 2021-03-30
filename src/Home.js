2035060a8ef87965059367b741a879908939cd5e
limonsafayet <limon6531@gmail.com>
2021-03-30 15:34:34 +0600

    tutorial 21

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,17 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList.js";
+import useFetch from "./useFetch";
+
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-  
-    useEffect(() => {
-      setTimeout(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          setIsPending(false);
-          setBlogs(data);
-        }).catch(err =>{
-            console.log(err.message);
-        })
-      }, 1000);
-    }, [])
-  
-    return (
-      <div className="home">
-        { isPending && <div>Loading...</div> }
-        {blogs && <BlogList blogs={blogs} />}
-      </div>
-    );
-  }
-   
-  export default Home;
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
+}
+ 
+export default Home;
